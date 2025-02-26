75bfdd50b9b33002587733cccf5b9fa9ce017c94
ArbeChumala <arbe.chumala5407@gmail.com>
2025-02-26 14:15:23 -0600

    started ball movement

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,14 +6,38 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let dx;
+let dy;
+let x;
+let y;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  noStroke();
+  x = width/2;
+  y = width/2;
+  dx = random(5,20);
+  dy = random(3,10);
 }
 
 function draw() {
+  background(0);
+  checkCollisions();
   moveBall();
+  displayBall();
+}
+
+function checkCollisions(){
+  if (y>= width || y <= 0){
+    dy *= -1;
+  }
 }
 
 function moveBall(){
-  
+  x += dx;
+  y += dy;
+}
+
+function displayBall(){
+  circle(x, y, 20);
 }
