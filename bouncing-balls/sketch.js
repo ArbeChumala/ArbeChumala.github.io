b8dd9d1da4ea67c8d543f2118f2273811dab03ff
ArbeChumala <arbe.chumala5407@gmail.com>
2025-03-05 14:15:04 -0600

    ayayyy demooo

---
+++ bouncing-balls/sketch.js
@@ -0,0 +1,48 @@
+// Bouncing Ball Object Demo
+
+let ballArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  spawnBall();
+}
+
+function draw() {
+  background(220);
+
+  for (let ball of ballArray){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if (ball.x > width){
+      ball.x -= width;
+    }
+    else if (ball.x < 0){
+      ball.x += width;
+    }
+    if (ball.y > height){
+      ball.y -= height;
+    }
+    else if (ball.y < 0){
+      ball.y += height;
+    }
+
+    fill("red");
+    circle(ball.x, ball.y, 2*ball.radius);
+  }
+}
+
+function mousePressed(){
+  spawnBall();
+}
+
+function spawnBall(){
+  let someBall = {
+    x: random(width),
+    y: random(height),
+    radius: random(15,40),
+    dx: random(-5,5),
+    dy: random(-5,5),
+  };
+  ballArray.push(someBall);
+}
--- index.md
+++ index.md
@@ -1,5 +1,7 @@
 # Cs30 Demos
 - [Color Time Swapper](color-time-swapper)
 - [Traffic Light Simulator](traffic-light-simulator)
+- [Bouncing Balls](bouncing-balls)
+
 # Cs30 Projects
 - [Interactive Scene](interactive-scene/)
