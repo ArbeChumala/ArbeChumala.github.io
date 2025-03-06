d3c4e7ce71d999114726ffdd9b5adf3ff144d7d2
ArbeChumala <arbe.chumala5407@gmail.com>
2025-03-06 13:59:45 -0600

    objects and stuff

--- bouncing-balls/sketch.js
+++ bouncing-balls/sketch.js
@@ -9,40 +9,46 @@ function setup() {
 
 function draw() {
   background(220);
-
+  spawnBall();
   for (let ball of ballArray){
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-
-    if (ball.x > width){
-      ball.x -= width;
-    }
-    else if (ball.x < 0){
-      ball.x += width;
-    }
-    if (ball.y > height){
-      ball.y -= height;
-    }
-    else if (ball.y < 0){
-      ball.y += height;
-    }
-
-    fill("red");
-    circle(ball.x, ball.y, 2*ball.radius);
+    moveBalls(ball);
+    displayBalls(ball);
   }
 }
 
-function mousePressed(){
-  spawnBall();
+function moveBalls(ball){
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  if (ball.x - ball.radius > width){
+    ball.x = - ball.radius;
+  }
+  else if (ball.x + ball.radius < 0 ){
+    ball.x = width + ball.radius;
+  }
+  if (ball.y - ball.radius > height){
+    ball.y = - ball.radius;
+  }
+  else if (ball.y + ball.radius < 0){
+    ball.y = height + ball.radius;
+  }
 }
 
+function displayBalls(ball){
+  fill("red");
+  circle(ball.x, ball.y, 2*ball.radius);
+}
+
+
 function spawnBall(){
-  let someBall = {
-    x: random(width),
-    y: random(height),
-    radius: random(15,40),
-    dx: random(-5,5),
-    dy: random(-5,5),
-  };
-  ballArray.push(someBall);
+  if (mouseIsPressed){
+    let someBall = {
+      x: random(width),
+      y: random(height),
+      radius: random(15,40),
+      dx: random(-5,5),
+      dy: random(-5,5),
+    };
+    ballArray.push(someBall);
+  }
 }
