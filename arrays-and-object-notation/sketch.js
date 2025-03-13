e76f7c534d87fd483682997e363acd3618782548
ArbeChumala <arbe.chumala5407@gmail.com>
2025-03-12 22:47:53 -0600

    added shuffling function

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -5,25 +5,37 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let cardList = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  generateCards();
   shuffleCards();
 }
 
 function draw() {
   background(220);
-  
+}
+
+function shuffleCards(){
+  for (let i = cardList.length - 1; i>=0; i--){
+    newestIndex = Math.floor(random(0,i));
+    let newestSelection = cardList[newestIndex];
+    let selectionToMove = cardList[i];
+    cardList[newestIndex] = selectionToMove;
+    cardList[i] = newestSelection;
+  }
 }
 
 function generateCards(){
   suitList = ["clubs", "spades", "hearts", "diamonds"];
   for (let theSuit of suitList){
-    for (let cardNumber = 0; cardNumber<=14; cardNumber++){
+    for (let cardNumber = 0; cardNumber<=13; cardNumber++){
       let myCard = {
         suit: theSuit,
-        number: card
+        number: cardNumber,
       };
+      cardList.push(myCard);
     } 
   }
 }
