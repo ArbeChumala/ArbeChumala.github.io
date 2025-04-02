056786e18f7468431fff5320744a5ea2ad2ba419
ArbeChumala <arbe.chumala5407@gmail.com>
2025-04-02 14:15:29 -0600

    finally finished the findMoves() function and allowed players to play in the console

--- reversi/sketch.js
+++ reversi/sketch.js
@@ -5,16 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let EMPTY = 9;
-let WHITE = 0;
-let BLACK = 1;
+let EMPTY = 0;
+let WHITE = 1;
+let BLACK = 2;
 
 let GRID_DIMENSIONS = 8;
 
 let whiteIsPlaying = false;
 
 let grid = generateGrid();
+let movesArray;
+let currentPlayer = BLACK;
+let whiteTiles;
+let blackTiles;
 
+function toggleCurrentPlayer(){
+  currentPlayer = currentPlayer - 1 ? WHITE: BLACK;
+  
+  if (!findMoves(currentPlayer)){
+    toggleCurrentPlayer();
+  }
+}
 
 function generateGrid(){
   let newGrid = [];
@@ -43,8 +54,17 @@ function draw() {
   background(220);
 }
 
-function findMoves(currentPlayer) {
-  let movesArray = [];
+function findMoves() {
+  let moveFound = false;
+  movesArray = [];
+  for (let y = 0; y<GRID_DIMENSIONS; y++){
+    movesArray.push([]);
+
+    for (let x = 0; x<GRID_DIMENSIONS; x++){
+      movesArray[y].push(0);
+    }
+  }
+  
 
   for (let y = 0; y<GRID_DIMENSIONS; y++){
     for (let x = 0; x<GRID_DIMENSIONS; x++){
@@ -57,16 +77,42 @@ function findMoves(currentPlayer) {
           let counter = 1;
 
           //place you're looking is not white or white is Playing
-          while (grid[y+iy[i]*counter][x+ix[i]*counter] !== currentPlayer && grid[y+iy[i]*counter][x+ix[i]*counter] !== EMPTY && grid[y+iy[i]*counter][x+ix[i]*counter] !== 2){
+          while (grid[y+iy[i]*counter][x+ix[i]*counter] !== currentPlayer && grid[y+iy[i]*counter][x+ix[i]*counter] !== EMPTY){
             counter++;
           }
-          if(grid[y+iy[i]*counter][x+ix[i]*counter] === EMPTY){
-            grid[y+iy[i]*counter][x+ix[i]*counter] = 2;
+          if(grid[y+iy[i]*counter][x+ix[i]*counter] === EMPTY && counter>1){
+            movesArray[y+iy[i]*counter][x+ix[i]*counter] += counter-1;
+            moveFound = true;
           }
-          
         }
       }
     }
   }
-  return movesArray;
+  return moveFound;
+}
+
+function playerMoves(x, y){
+  findMoves(currentPlayer);
+
+  if (movesArray[y][x]){
+    let ix = [1, 1, 0, -1, -1, -1, 0, 1];
+    let iy = [0, 1, 1, 1, 0, -1, -1, -1];
+    
+    for (let i = 0; i<8; i++){
+      let counter = 1;
+
+      //place you're looking for is the current player
+      while (grid[y+iy[i]*counter][x+ix[i]*counter] !== currentPlayer && grid[y+iy[i]*counter][x+ix[i]*counter] !== EMPTY){
+        counter++;
+      }
+      //you have stopped and can only mark it if it is empty
+      if(grid[y+iy[i]*counter][x+ix[i]*counter] === currentPlayer && counter>1){
+        for (counter; counter >=0; counter --){
+          grid[y+iy[i]*counter][x+ix[i]*counter] = currentPlayer;
+        }
+      }      
+    }
+
+    toggleCurrentPlayer();
+  }
 }
