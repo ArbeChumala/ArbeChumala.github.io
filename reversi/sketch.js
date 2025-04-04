bbd65d46b4f229d9a88719927afb6e73f01c4aa1
ArbeChumala <arbe.chumala5407@gmail.com>
2025-04-03 23:45:10 -0600

    added basic graphics and mousePressed() funcitonality

--- reversi/sketch.js
+++ reversi/sketch.js
@@ -15,9 +15,21 @@ let whiteIsPlaying = false;
 
 let grid = generateGrid();
 let movesArray;
-let currentPlayer = BLACK;
-let whiteTiles;
-let blackTiles;
+let currentPlayer = WHITE;
+
+let whiteTile;
+let blackTile;
+let blackGhostTile;
+let whiteGhostTile;
+
+let resizingRatio;
+let cellSize;
+let aisleSize;
+let startingImageX;
+let startingImageY;
+let startingMouseX;
+let startingMouseY;
+let gridUnit;
 
 function toggleCurrentPlayer(){
   currentPlayer = currentPlayer - 1 ? WHITE: BLACK;
@@ -48,6 +60,26 @@ function generateGrid(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  toggleCurrentPlayer();
+  noSmooth();
+  imageMode(CENTER);
+  resizingRatio = height/228;
+  cellSize = 18*resizingRatio;
+  aisleSize = 1*resizingRatio;
+  gridUnit = aisleSize + cellSize;
+  startingImageX = width/2 - 3.5*gridUnit;
+  startingImageY = height/2 - 3.5*gridUnit;
+  startingMouseX = startingImageX - 0.5*cellSize;
+  startingMouseY = startingImageY - 0.5*cellSize;
+  
+}
+
+function preload(){
+  board = loadImage("assets/board.png");
+  blackTile = loadImage("assets/black-tile.png");
+  whiteTile = loadImage("assets/white-tile.png");
+  whiteGhostTile = loadImage("assets/ghost-white-tile.png");
+  blackGhostTile = loadImage("assets/ghost-black-tile.png");
 }
 
 function draw() {
@@ -56,8 +88,37 @@ function draw() {
 }
 
 function displayGrid(){
-  //display the grid at the centre of the canvas
-  //display the tiles on the grid
+  image(board, width/2, height/2, board.width*resizingRatio, board.height*resizingRatio);
+
+  for (let y = 0; y<GRID_DIMENSIONS; y++){
+    for(let x = 0; x<GRID_DIMENSIONS; x++){
+
+      if (grid[y][x]===BLACK){
+        image(blackTile, startingImageX+x*gridUnit, startingImageY+y*gridUnit, blackTile.width*resizingRatio, blackTile.height*resizingRatio);
+      }
+      else if (grid[y][x]===WHITE){
+        image(whiteTile, startingImageX+x*gridUnit, startingImageY+y*gridUnit, whiteTile.width*resizingRatio, whiteTile.height*resizingRatio);
+      }
+
+      if (movesArray[y][x]){
+        let theImage = currentPlayer - 1 ? blackGhostTile: whiteGhostTile;
+
+        image(theImage,startingImageX+x*gridUnit, startingImageY+y*gridUnit, theImage.width*resizingRatio, theImage.height*resizingRatio);
+      }
+      
+    }
+  }
+}
+
+function mousePressed(){
+  let playerX = Math.floor((mouseX-startingMouseX)/gridUnit);
+  let playerY = Math.floor((mouseY-startingMouseY)/gridUnit);
+
+  console.log(playerY, playerX);
+
+  if (playerX >=0 && playerX <GRID_DIMENSIONS && playerY >=0 && playerY <GRID_DIMENSIONS){
+    playerMoves(playerX, playerY);
+  }
 }
 
 function findMoves() {
@@ -98,8 +159,6 @@ function findMoves() {
 }
 
 function playerMoves(x, y){
-  findMoves(currentPlayer);
-
   if (movesArray[y][x]){
     let ix = [1, 1, 0, -1, -1, -1, 0, 1];
     let iy = [0, 1, 1, 1, 0, -1, -1, -1];
@@ -108,11 +167,18 @@ function playerMoves(x, y){
       let counter = 1;
 
       //place you're looking for is the current player
-      while (grid[y+iy[i]*counter][x+ix[i]*counter] !== currentPlayer && grid[y+iy[i]*counter][x+ix[i]*counter] !== EMPTY){
+      while (y+iy[i]*counter >=0 && y+iy[i]*counter < GRID_DIMENSIONS &&
+             x+ix[i]*counter >=0 && x+ix[i]*counter < GRID_DIMENSIONS &&
+             grid[y+iy[i]*counter][x+ix[i]*counter] !== currentPlayer && 
+             grid[y+iy[i]*counter][x+ix[i]*counter] !== EMPTY){
         counter++;
+        console.log(counter);
       }
       //you have stopped and can only mark it if it is empty
-      if(grid[y+iy[i]*counter][x+ix[i]*counter] === currentPlayer && counter>1){
+      if(y+iy[i]*counter >=0 && y+iy[i]*counter < GRID_DIMENSIONS &&
+         x+ix[i]*counter >=0 && x+ix[i]*counter < GRID_DIMENSIONS &&
+         grid[y+iy[i]*counter][x+ix[i]*counter] === currentPlayer && 
+         counter>1){
         for (counter; counter >=0; counter --){
           grid[y+iy[i]*counter][x+ix[i]*counter] = currentPlayer;
         }
