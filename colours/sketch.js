dd4be2ee43b67a79aeac2a7e2a724b1cd23e8151
ArbeChumala <arbe.chumala5407@gmail.com>
2025-05-30 12:19:26 -0600

    change to background

--- colours/sketch.js
+++ colours/sketch.js
@@ -13,8 +13,8 @@ function setup() {
 }
 
 function draw() {
-  r = g === 255 ? r === 255 ? r : r+5 : r;
-  g = b === 255 ? g === 255 ? g : g+5 : g;
-  b = b === 255 ? b: b+5;
+  r = g !== 255 || r === 255 ? r : r+1;
+  g = b !== 255 || g === 255 ? g : g+1;
+  b = b === 255 ? 255: b+1;
   background(r, g, b);
 }
